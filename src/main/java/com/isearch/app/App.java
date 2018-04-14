b203d578412abf97ba1e573f1f493d4e8014bea9
Stefan Zhelyazkov <stz@alumni.upenn.edu>
2018-04-14 12:42:16 -0400

    FEAT - Add Correct Gradle Files

---
+++ src/main/java/com/isearch/app/App.java
@@ -0,0 +1,15 @@
+package com.isearch.app;
+
+public class App {
+
+    public static void main(String[] args) {
+        if (args == null) {
+            System.out.println("You need to provide");
+        }
+        for (String arg : args) {
+
+        }
+        System.out.println("Hello World!");
+    }
+
+}
